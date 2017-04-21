9f7f15a530312375b4f3ebd47c8c00d7e3372ba7
Fishbiao <313744171@qq.com>
2017-04-21 22:40:12 +0800

    整理一下

--- authServer/app.js
+++ authServer/app.js
@@ -21,6 +21,16 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 // development only
 if ('development' === app.get('env')) {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
